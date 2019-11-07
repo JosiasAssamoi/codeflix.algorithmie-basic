4096ea75bffa562f23e1773fde18da65b7e4625b
JosiasAssamoi <assamoi.josias@gmail.com>
2019-11-07 16:27:00 +0100

    Ep006

---
+++ episode.006/src/pangram.js
@@ -0,0 +1,16 @@
+export default function pangram(sentence) {
+
+   sentence= sentence.toLowerCase()
+    let alphabet= []
+    for(let i=97;i<123;i++){
+        alphabet.push(String.fromCharCode(i))
+    }
+    for (let lettre of alphabet){
+        if(!(sentence.includes(lettre))){
+            return false
+        }
+    }
+    return true
+
+
+}
---
+++ episode.008/src/matrix.js
@@ -0,0 +1,23 @@
+export default function strToMatrix(mat=""){
+   
+    let matrice = {
+        col:null,
+        rows:null,
+    }
+
+  
+    let cols = mat.split(' ')
+
+
+    console.log("ma col",cols)
+    
+
+    /*myMat = myMat.map(function(elt){
+        return parseInt(elt)
+    })*/
+
+
+    return matrice
+}
+
+// 1 
